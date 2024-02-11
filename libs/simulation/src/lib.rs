a3732e3eda37cc72793c66e30b42cfcbad1d2e19
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2024-02-10 19:41:29 -0800

    add simulation layer

---
+++ libs/simulation/src/lib.rs
@@ -0,0 +1,77 @@
+use nalgebra as na;
+use rand::{Rng, RngCore};
+
+pub struct Simulation {
+    world: World,
+}
+
+#[derive(Debug)]
+pub struct World {
+    animals: Vec<Animal>,
+    foods: Vec<Food>,
+}
+
+#[derive(Debug)]
+pub struct Animal {
+    position: na::Point2<f32>,
+    rotation: na::Rotation2<f32>,
+    speed: f32,
+}
+
+#[derive(Debug)]
+pub struct Food {
+    position: na::Point2<f32>,
+}
+
+impl Simulation {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            world: World::random(rng),
+        }
+    }
+    pub fn world(&self) -> &World {
+        &self.world
+    }
+}
+
+impl World {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        let animals = (0..40).map(|_| Animal::random(rng)).collect();
+        let foods = (0..60).map(|_| Food::random(rng)).collect();
+
+        Self { animals, foods }
+    }
+    pub fn animals(&self) -> &[Animal] {
+        &self.animals
+    }
+    pub fn foods(&self) -> &[Food] {
+        &self.foods
+    }
+}
+
+impl Animal {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            position: rng.gen(),
+            rotation: rng.gen(),
+            speed: 0.002,
+        }
+    }
+    pub fn position(&self) -> na::Point2<f32> {
+        self.position
+    }
+    pub fn rotation(&self) -> na::Rotation2<f32> {
+        self.rotation
+    }
+}
+
+impl Food {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            position: rng.gen(),
+        }
+    }
+    pub fn position(&self) -> na::Point2<f32> {
+        self.position
+    }
+}
